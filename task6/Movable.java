4348bf0c7a534ef9a97ad5ca5cad340b6182ae3f
Kanan Hajiyev <haciyevkenan2017@gmail.com>
2026-03-06 10:43:58 +0400

    Update Movable.java

--- task6/Movable.java
+++ task6/Movable.java
@@ -1,20 +1,5 @@
 package task6;
 
-//Movable interface
-// a. Define an interface Movable with four methods:
-// i. moveUp
-// ii. moveDown
-// iii. moveLeft
-// iv. moveRight
-// b. Take the Point class and implement Movable interface so that any object of the point
-// can be moved to al four directions on a 2D coordinate system.
-// c. Apply the same to Segment class.
-// d. Define a Circle class which is represented by the center point and a radius.
-// i. Make Circle implement Movable.
-// e. Test the added behavior.
-// f. Create an array consisting of a mixture of Points, Segments, Circles in a Main class.
-// g. Write a method that will take that array as input and move all of them to any directio
-
 public interface Movable {
     void moveUp();
     void moveDown();
